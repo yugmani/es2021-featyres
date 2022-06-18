124d9e567217498a0457b7e2eca55a075fafe984
yugmani <yugmani@yahoo.com>
2022-06-18 16:25:21 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-abwcsd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-abwcsd)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// String replaceAll() method
+// ****************************************
+
+let str = 'a-b-c-d';
+
+let newStr = str.replaceAll('-', ' ');
+
+console.log(newStr);
+// a b c d
+
+
